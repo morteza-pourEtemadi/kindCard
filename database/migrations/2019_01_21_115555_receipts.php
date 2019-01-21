260703a1d77b1657270e28e96419e2f4d3daae5c
morteza <e.morteza94@yahoo.com>
2019-01-21 16:10:28 +0330

    Initial Commit

---
+++ database/migrations/2019_01_21_115555_receipts.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Receipts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id', false, true)->index('');
+            $table->text('transactions')->nullable();
+            $table->bigInteger('last_transaction_id', false, true)->nullable();
+            $table->integer('price');
+            $table->integer('status');
+            $table->timestamp('payed_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receipts');
+    }
+}
